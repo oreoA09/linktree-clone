a8b239503aaa9f4a19eefa95125ea61c6e7e7cd1
oreoA09 <oreo.akanni@gmail.com>
2022-10-26 17:27:02 +0000

    adding images & creating links.js

---
+++ src/links.js
@@ -0,0 +1,45 @@
+export const links = [
+    {
+    id: 1,
+    url:
+        'https://twitter.com/',
+    title: 'Twitter',
+    desc: 'Connect with me on Twitter'
+    },
+    {
+     id: 2,
+     url:
+      'https://training.zuri.team/',
+     title: 'Zuri Team',
+     desc: 'Meet the Zuri Team'
+    },
+    {
+    id: 3,
+    url:
+    'http://books.zuri.team/',
+    title: 'Zuri Books',
+    desc: 'Find books about design and coding online at Zuri Books'
+    },
+    {
+    id: 4,
+    url:
+        'https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>',
+    title: 'Python Books',
+    desc: 'E-books "Python for Beginners" for just $13'
+    },
+    {
+    id: 5,
+    url:
+        'https://background.zuri.team/',
+    title: 'Background Check for Coders',
+    desc: 'Do background checks on coders you want to hire'
+    },
+    {
+    id: 6,
+    url:
+        'https://books.zuri.team/design-rules',
+    title: 'Design Books',
+    desc: 'Get your copy of the free design book offered by Zuri'
+    },
+   ];
+   
